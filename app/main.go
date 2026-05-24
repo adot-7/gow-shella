780183c0d0dfc99253aed458bb7ed350ae6d5458
adot-7 <akashk.projects@gmail.com>
2026-05-24 23:31:28 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,17 +14,30 @@ func main() {
 		fmt.Print("$ ")
 		acceptableCommands := make([]string, 0)
 		acceptableCommands = append(acceptableCommands, "exit")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		acceptableCommands = append(acceptableCommands, "echo")
+		reader := bufio.NewReader(os.Stdin)
+		command, err := reader.ReadString(' ')
 		if err != nil {
 			panic(err)
 		}
-		command = strings.TrimSuffix(command, "\n")
+		arguments, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		arguments = strings.TrimSuffix(arguments, "\n")
+		command = strings.TrimSuffix(command, " ")
+		// fmt.Printf("arguments: %s\ncommand: %s\n", arguments, command)
+		// argumentsSlice := strings.Split(arguments, " ")
+		// argumentsSlice = append(argumentsSlice, argumentsSlice...)
+
 		if !slices.Contains(acceptableCommands, command) {
 			fmt.Printf("%s: command not found\n", command)
 		}
 		switch command {
 		case "exit":
 			loop = false
+		case "echo":
+			fmt.Printf("%s\n", arguments)
 		}
 	}
 }
