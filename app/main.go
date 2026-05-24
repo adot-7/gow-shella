60773c96f12d3d7ec32af79e34faadbf5b0664b9
adot-7 <akashk.projects@gmail.com>
2026-05-24 23:47:30 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -16,28 +16,35 @@ func main() {
 		acceptableCommands = append(acceptableCommands, "exit")
 		acceptableCommands = append(acceptableCommands, "echo")
 		reader := bufio.NewReader(os.Stdin)
-		command, err := reader.ReadString(' ')
+		input, err := reader.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
-		arguments, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
-		arguments = strings.TrimSuffix(arguments, "\n")
-		command = strings.TrimSuffix(command, " ")
+		input = strings.TrimSuffix(input, "\n")
+		inputs := strings.SplitN(input, " ", 2)
+
+		command := inputs[0]
+		// command = strings.TrimSuffix(command, " ")
+		// arguments, err := reader.ReadString('\n')
 		// fmt.Printf("arguments: %s\ncommand: %s\n", arguments, command)
 		// argumentsSlice := strings.Split(arguments, " ")
 		// argumentsSlice = append(argumentsSlice, argumentsSlice...)
-
+		// fmt.Printf("%s\n%s\n", command, arguments)
 		if !slices.Contains(acceptableCommands, command) {
 			fmt.Printf("%s: command not found\n", command)
+			continue
 		}
 		switch command {
 		case "exit":
 			loop = false
 		case "echo":
-			fmt.Printf("%s\n", arguments)
+			if len(inputs) == 2 {
+				arguments := inputs[1]
+				fmt.Printf("%s\n", arguments)
+			} else {
+				fmt.Printf("\n")
+			}
+
 		}
 	}
 }
