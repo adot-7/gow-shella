139b56960475f3b71e14f940fea8dc870e1bf9ac
adot-7 <akashk.projects@gmail.com>
2026-05-24 22:55:59 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"slices"
+	"strings"
 )
-
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+	acceptableCommands := make([]string, 0)
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	command = strings.TrimSuffix(command, "\n")
+	if !slices.Contains(acceptableCommands, command) {
+		fmt.Printf("%s: command not found\n", command)
+	}
 }
