3c023f63b0dbe640efe0265ccda1f38be67c12d7
adot-7 <akashk.projects@gmail.com>
2026-06-02 06:30:57 +0530

    codecrafters submit [skip ci]

--- readline/complete.go
+++ readline/complete.go
@@ -66,8 +66,7 @@ func (o *opCompleter) nextCandidate(i int) {
 }
 
 func (o *opCompleter) OnComplete() bool {
-	if o.width == 0 || !tabState {
-		tabState = !tabState
+	if o.width == 0 {
 		return false
 	}
 	if o.IsInCompleteSelectMode() {
@@ -107,7 +106,10 @@ func (o *opCompleter) OnComplete() bool {
 			return true
 		}
 	}
-
+	if !tabState {
+		tabState = !tabState
+		return false
+	}
 	o.EnterCompleteMode(offset, newLines)
 	// o.ExitCompleteMode(false) //EXITED COMPLETE MODE TO NOT ENTER SELECT MODE AND INSTEAD MOVE TO NEXT LINE ACCORDING TO CONDITIONS OF BYOX CHALLENGE
 	// o.op.buf.print()          // prints the buffer from stdin ig, before the writes of completemode, the actual prompt along with '$ '
